b8cd95cf1204a4e553adc3df67074c1b4da6c9db
Ekaterina Shum <ekaterina@plantminer.com.au>
2016-10-27 11:08:29 +1000

    Fixed redux extension as per boilerplate PM-1017

--- category-selector/src/index.js
+++ category-selector/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers/index';
@@ -9,11 +9,16 @@ import api from '../../shared/api.config';
 import CategorySelection from './containers/CategorySelection';
 import './styles/index.scss';
 
+// Add redux dev tools unless we have a production build
+const enhance = process.env.NODE_ENV !== 'production' && window.devToolsExtension ? compose(
+    applyMiddleware(thunkMiddleware),
+    window.devToolsExtension && window.devToolsExtension()
+) : applyMiddleware(thunkMiddleware);
+
 // Configure store with thunk middleware to allow async requests
 const store = createStore(
     rootReducer,
-    window.devToolsExtension && window.devToolsExtension(),
-    applyMiddleware(thunkMiddleware)
+    enhance
 );
 
 store.dispatch(setEndpointHost(api.configureHostname()));
@@ -24,5 +29,5 @@ render(
     <Provider store={store}>
         <CategorySelection/>
     </Provider>,
-    document.querySelector('[data-component="category-selector"')
+    document.querySelector('[data-component="category-selector"]')
 );
