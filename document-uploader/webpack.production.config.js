15dbf3af15f23401813a55def413101fd8fe8335
Ryan <ryan@plantminer.com.au>
2016-12-01 12:33:52 +0800

    PM-866
     - Production config updated from master

--- document-uploader/webpack.production.config.js
+++ document-uploader/webpack.production.config.js
@@ -1,12 +1,10 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const vendorPlugins = require('../shared/vendorPlugins.js');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        vendor: vendorPlugins
+        app: './src/index.js'
     },
     devtool: 'cheap-module-source-map',
     output: {
@@ -56,6 +54,12 @@ module.exports = {
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.DedupePlugin(),
         new ExtractTextPlugin('./index.css'),
-        new webpack.optimize.CommonsChunkPlugin('vendor', '../../shared/dist/vendor.bundle.js')
+        // In order to reduce the size of app bundle
+        // We are using a dll library reference to point to a pre build vendor bundle
+        // VWhen vendor bundle is rebuild - all apps have to rebuild to match the changes
+        new webpack.DllReferencePlugin({
+            context: '.',
+            manifest: require('../shared/dist/vendor-manifest.json')
+        })
     ]
 };
