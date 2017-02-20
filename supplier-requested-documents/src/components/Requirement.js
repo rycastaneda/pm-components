8d5e2a6f3eaf2eab857ceb55184075c26127a6d0
Ryan <ryan@plantminer.com.au>
2017-02-20 16:04:49 +0800

    PM-867
     - updated class and styles

--- supplier-requested-documents/src/components/Requirement.js
+++ supplier-requested-documents/src/components/Requirement.js
@@ -19,17 +19,16 @@ class Requirement extends Component {
 
         return (
             <li>
-                <ul className="list-group">
-                    <li className="list-group-item">
-                        <Dropzone className="dropzone clearfix"
+                <ul>
+                    <li>
+                        <Dropzone className="dropzone dz-clickable clearfix"
                             accept="application/pdf"
                             onDrop={(documents) => {
                                 onDropDocuments(requirement.id, documents);
                             }}>
-                            <div className="pull-left">
-                                {requirement.title}
-                            </div>
-                            <div className="pull-right">
+                            <div className="form-group">
+                                <label className="pull-left">{requirement.title}</label>
+                                <div className="clearfix"></div>
                                 <p className="text-center dropzone__placeholder">
                                     <i className="fa fa-cloud-upload"></i> Drop documents here or click to select files.
                                 </p>
