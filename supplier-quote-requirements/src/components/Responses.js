3b1b1a611aa58a91484f19e0fea8dc3792b50400
Ryan <ryan@plantminer.com.au>
2017-02-28 15:27:01 +0800

    PM-867: Added button type to prevent form from submitting

--- supplier-quote-requirements/src/components/Responses.js
+++ supplier-quote-requirements/src/components/Responses.js
@@ -2,15 +2,18 @@ import React, { PropTypes } from 'react';
 
 const Responses = ({ updateSelection, response }) => (
     <div>
-        <button 
+        <button
+            type="button" 
             className={`btn display-form__button${response === 'yes' ? ' display-form__button--selected btn-success' : ''}`}
             onClick={() => updateSelection('yes')}>Yes
         </button>
-        <button 
+        <button
+            type="button" 
             className={`btn display-form__button${response === 'no' ? ' display-form__button--selected btn-success' : ''}`}
             onClick={() => updateSelection('no')}>No
         </button>
-        <button 
+        <button
+            type="button" 
             className={`btn display-form__button${response === 'n/a' ? ' display-form__button--selected btn-success' : ''}`}
             onClick={() => updateSelection('n/a')}>N/A
         </button>
