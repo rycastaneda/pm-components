bc9785add7d20e62748c379008aa9cf9690e225a
PlantMiner <mikean@plantminer.com.au>
2017-03-22 15:17:52 +1000

    PM-867 quote-requirements component with specs updated

--- quote-requirements/src/components/InclusionsSelection.spec.js
+++ quote-requirements/src/components/InclusionsSelection.spec.js
@@ -23,39 +23,54 @@ const options = [
 ];
 
 describe('Inclusions selection component: ', () => {
-    it('should render select wrapper and default 2 options with \'none\' being default value', () => {
-        const { component } = setup({ handleChange });
+    it('should render select wrapper and default 3 options with \'onlyService\' being default value', () => {
+        const { component } = setup({ handleChange, quote_request_id: '1234' });
 
         expect(component.node.type).to.equal('select');
         expect(component.props().onChange).to.equal(handleChange);
-        expect(component.props().children.length).to.equal(3);
+        expect(component.props().children.length).to.equal(4);
         expect(component.props().children[0].type).to.equal('option');
         expect(component.props().children[1].type).to.equal('option');
-        expect(component.props().children[2]).to.equal(null);
-        expect(component.props().defaultValue).to.equal('none');
+        expect(component.props().children[2].type).to.equal('option');
+        expect(component.props().children[3]).to.equal(null);
+        expect(component.props().defaultValue).to.equal('onlyService');
     });
 
-    it('should render select wrapper and default 2 options with \'all\' being default value', () => {
-        const { component } = setup({ handleChange, include: true });
+    it('should render select wrapper and default 3 options with \'onlyQR\' being default value', () => {
+        const { component } = setup({ handleChange, include: false, quote_request_id: 'null' });
 
         expect(component.node.type).to.equal('select');
         expect(component.props().onChange).to.equal(handleChange);
-        expect(component.props().children.length).to.equal(3);
+        expect(component.props().children.length).to.equal(4);
         expect(component.props().children[0].type).to.equal('option');
         expect(component.props().children[1].type).to.equal('option');
-        expect(component.props().children[2]).to.equal(null);
+        expect(component.props().children[2].type).to.equal('option');
+        expect(component.props().children[3]).to.equal(null);
+        expect(component.props().defaultValue).to.equal('onlyQR');
+    });
+
+    it('should render select wrapper and default 3 options with \'all\' being default value', () => {
+        const { component } = setup({ handleChange, include: true, quote_request_id: 'null' });
+
+        expect(component.node.type).to.equal('select');
+        expect(component.props().onChange).to.equal(handleChange);
+        expect(component.props().children.length).to.equal(4);
+        expect(component.props().children[0].type).to.equal('option');
+        expect(component.props().children[1].type).to.equal('option');
+        expect(component.props().children[2].type).to.equal('option');
+        expect(component.props().children[3]).to.equal(null);
         expect(component.props().defaultValue).to.equal('all');
     });
 
-    it('should render select wrapper and default 2 options plus an additional option passed as param', () => {
+    it('should render select wrapper and default 3 options plus an additional option passed as param', () => {
         const { component } = setup({ handleChange, category_id: '22', include: true, options });
 
         expect(component.node.type).to.equal('select');
-        expect(component.props().children.length).to.equal(3);
-        expect(component.props().children[2].length).to.equal(1);
-        expect(component.props().children[2][0].type).to.equal('option');
-        expect(component.props().children[2][0].props.value).to.equal('22');
-        expect(component.props().children[2][0].props.children).to.equal('any test');
+        expect(component.props().children.length).to.equal(4);
+        expect(component.props().children[3].length).to.equal(1);
+        expect(component.props().children[3][0].type).to.equal('option');
+        expect(component.props().children[3][0].props.value).to.equal('22');
+        expect(component.props().children[3][0].props.children).to.equal('any test');
         expect(component.props().defaultValue).to.equal('22');
     });
 });
