196baaa9050ef5daa87aaa5e6664b2c69bbfd194
Torsten Schmidt <Torsten@plantminer.com.au>
2017-11-20 22:39:36 +1000

    PIT-482: updated dataParser.js for new cases of questionnaires

--- searcher-evaluation-submission/src/utils/dataParser.js
+++ searcher-evaluation-submission/src/utils/dataParser.js
@@ -4,6 +4,7 @@ export function getSectionsFromService(data) {
         sections:{ experience:{ id:'experience', label: 'Experience', weighting:'30', questionnaire:[{
             id:1,
             label:'Has the tenderer demonstrated experience in successfully delivering similar work?',
+            type: 'vertical-scale',
             options:[
                 { id:0, label: 'The tenderer is beyond terrible', score:1 },
                 { id:1, label: 'The tenderer has completed work to a poor standard', score:2 },
@@ -16,9 +17,23 @@ export function getSectionsFromService(data) {
                 { id:0, label:'project-specs.pdf', url:'/Attachment' },
                 { id:1, label:'map-of-bundy.jpg', url:'/Attachment' },
                 { id:2, label:'key-outcome.xls', url:'/Attachment' }
-            ]
-        }] } },
-        sectionsIds:['experience']
+            ],
+            id:2,
+            label:'The tenderer has demonstrated experience in successfully delivering similar work?',
+            type: 'multiple-choice',
+            options:[
+                { id:0, label: 'Yes' },
+                { id:1, label: 'No' }
 
+            ],
+            attachments:[
+                { id:0, label:'project-specs.pdf', url:'/Attachment' },
+                { id:1, label:'map-of-bundy.jpg', url:'/Attachment' },
+                { id:2, label:'key-outcome.xls', url:'/Attachment' }
+            ]
+        }]
+       }
+     },
+     sectionsIds:['experience']
     };
 }
