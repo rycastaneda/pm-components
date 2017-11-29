eea687cd23aaa8b560ea592a417e9e6090d8241f
Ryan <ryan@plantminer.com.au>
2017-11-29 14:48:32 +0800

    PIT-446: Change incorrect id value of declined

--- sign-offs-report/src/constants/ActionTypes.js
+++ sign-offs-report/src/constants/ActionTypes.js
@@ -16,7 +16,7 @@ export const API_ERROR = 'API_ERROR';
 export const PENDING = 0;
 export const IN_PROGRESS = 1;
 export const APPROVED = 2;
-export const DECLINED = 2;
+export const DECLINED = 3;
 
 export const options = [
     {
