582fefc0dddb16270d9b30ee47e6a6463e3b99ab
Ryan <ryan@plantminer.com.au>
2017-11-30 16:35:40 +0800

    PIT-460 Updates

--- searcher-evaluation-assignment-list/src/constants/DataConstants.js
+++
@@ -1,5 +0,0 @@
-export const STATUS_LIST = [{ id:null, label:'' }, { label:'Not Started', id:'not_started' }, { id:'submitted', label:'Submitted' }];
-export const LINKEDTO_LIST = [{ id:null, label:'' }, { label:'RFQ', id:'rfq' }, { id:'supplier', label:'Supplier' }, { id:'engagement', label:'Engagement' }];
-export const STATUS_DEFAULT = null;
-export const MAXROWS_LIST = [15, 30, 60];
-export const MAXROWS_DEFAULT = 15;
