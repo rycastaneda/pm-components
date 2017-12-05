1ed9c69b2f797022404697ce62bbe8dc305146de
Torsten Schmidt <torsten@plantminer.com.au>
2017-12-05 11:39:56 +1000

    PIT-468: Updated styling of date picker and form labels

--- searcher-evaluation-template-list/src/constants/DataConstants.js
+++ searcher-evaluation-template-list/src/constants/DataConstants.js
@@ -1,4 +1,4 @@
-export const STATUS_LIST = ['Show all', 'active', 'inactive'];
+export const STATUS_LIST = ['Any Status', 'active', 'inactive'];
 export const STATUS_DEFAULT = '';
 export const MAXROWS_LIST = [15, 30, 60];
 export const MAXROWS_DEFAULT =15;
